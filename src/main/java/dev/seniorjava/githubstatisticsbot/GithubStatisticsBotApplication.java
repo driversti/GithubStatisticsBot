144ccf5a543335735eb8138d58fe1a3d6e17f09c
driversti <driversti@gmail.com>
2021-09-29 12:01:14 +0200

    first commit

---
+++ src/main/java/dev/seniorjava/githubstatisticsbot/GithubStatisticsBotApplication.java
@@ -0,0 +1,13 @@
+package dev.seniorjava.githubstatisticsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubStatisticsBotApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GithubStatisticsBotApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/seniorjava/githubstatisticsbot/GithubStatisticsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.seniorjava.githubstatisticsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubStatisticsBotApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
